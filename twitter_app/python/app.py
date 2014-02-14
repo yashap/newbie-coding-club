fe221766fc37647b36611a3807c48fa9082e49c4
yashap <ypodeswa@gmail.com>
2014-02-13 18:12:45 -0800

    Switched to flask as our web server so we can deal with unicode

--- twitter_app/python/app.py
+++ twitter_app/python/app.py
@@ -1,6 +1,7 @@
 import time
 import BaseHTTPServer
 import urlparse
+import flask
 from TwitterSearch import *
 
 def twit_search(keywords):
@@ -39,43 +40,57 @@ def to_html(tweets):
     result.append("<p class='tweet'>"+tweet["tweet"]+"</p>"+"<h3 class='screen_name'>"+tweet["screen_name"]+"</h3>")
   return result
 
+PORT_NUMBER = 8000
 
-HOST_NAME = '0.0.0.0' # !!!REMEMBER TO CHANGE THIS!!!
-PORT_NUMBER = 8000 # Maybe set this to 9000.
+app = flask.Flask(__name__)
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-  # def do_HEAD(self):
-  #   self.send_response(200)
-  #   self.send_header("Content-type", "text/html")
-  #   self.end_headers()
-  def twitSearch(self):
-    self.send_response(200)
-    self.send_header("Content-type", "text/html")
-    self.end_headers()
-    self.wfile.write("<html><head><title>Title goes here.</title></head>")
-    self.wfile.write("<body>")
-    # If someone went to "http://something.somewhere.net/foo/bar/",
-    # then self.path equals "/foo/bar/".
-    keywords = urlparse.parse_qs(urlparse.urlparse(self.path).query)['keywords']
-    result = "\n".join(to_html(twit_search(keywords)))
-    self.wfile.write(result)
-    self.wfile.write("</body></html>")
-  def favicon(self):
-    self.send_response(404)
-  def do_GET(self):
-    """Respond to a GET request."""
-    if self.path == "/favicon.ico":
-      self.favicon()
+@app.route("/testing")
+def hello():
+    keywords = flask.request.args.getlist('keywords')
+    if keywords:
+      return "\n".join(to_html(twit_search(keywords)))
     else:
-      self.twitSearch()
+      return "Give me keywords!"
 
-if __name__ == '__main__':
-  server_class = BaseHTTPServer.HTTPServer
-  httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-  print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
-  try:
-    httpd.serve_forever()
-  except KeyboardInterrupt:
-    pass
-  httpd.server_close()
-  print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+if __name__ == "__main__":
+    app.run(port=PORT_NUMBER)
+
+# HOST_NAME = '0.0.0.0' # !!!REMEMBER TO CHANGE THIS!!!
+# PORT_NUMBER = 8000 # Maybe set this to 9000.
+
+# class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+#   # def do_HEAD(self):
+#   #   self.send_response(200)
+#   #   self.send_header("Content-type", "text/html")
+#   #   self.end_headers()
+#   def twitSearch(self):
+#     self.send_response(200)
+#     self.send_header("Content-type", "text/html")
+#     self.end_headers()
+#     self.wfile.write("<html><head><title>Title goes here.</title></head>")
+#     self.wfile.write("<body>")
+#     # If someone went to "http://something.somewhere.net/foo/bar/",
+#     # then self.path equals "/foo/bar/".
+#     keywords = urlparse.parse_qs(urlparse.urlparse(self.path).query)['keywords']
+#     result = "\n".join(to_html(twit_search(keywords)))
+#     self.wfile.write(result)
+#     self.wfile.write("</body></html>")
+#   def favicon(self):
+#     self.send_response(404)
+#   def do_GET(self):
+#     """Respond to a GET request."""
+#     if self.path == "/favicon.ico":
+#       self.favicon()
+#     else:
+#       self.twitSearch()
+
+# if __name__ == '__main__':
+#   server_class = BaseHTTPServer.HTTPServer
+#   httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+#   print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+#   try:
+#     httpd.serve_forever()
+#   except KeyboardInterrupt:
+#     pass
+#   httpd.server_close()
+#   print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
