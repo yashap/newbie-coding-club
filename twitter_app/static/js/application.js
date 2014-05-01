0fa31fd01baecad2bd71c0a72ca84e393b526ec2
yashap <ypodeswa@gmail.com>
2014-04-30 18:08:32 -0700

    Cleaned up JS to include very basic templating

--- twitter_app/static/js/application.js
+++ twitter_app/static/js/application.js
@@ -1,30 +1,43 @@
 $(function() {
 	$('.btn').on('click', function() {
 		$.getJSON('twitsearch?keywords=obama', '', function(myTweets) {
+			var i, k, tweet, text;
 			$('#_tweets').find('._row1').html('');
-			for (var i in myTweets) {
+			for (i in myTweets) {
 				if (i < 3) {
-					var tweet = myTweets[i];
-					$('#_tweets').find('._row1').append('<div class="col-md-4">' +
-							'<p>' + tweet.full_name + ' @' + tweet.screen_name + '</p>' +
-							'<p>' + tweet.text + '</p>' +
-							'<p>' + tweet.url + '</p>' +
-							'<p>' + tweet.created_at + '</p>' +
-						'</div>');
+					tweet = myTweets[i];
+					text = '<div class="col-md-4">' +
+						'<p>$full_name @$screen_name</p>' +
+						'<p>$text</p>' +
+						'<p>$url</p>' +
+						'<p>$created_at</p>' +
+						'</div>';
+					for (k in tweet) {
+						text = text.replace("$"+k, tweet[k]);
+					}
+					$('#_tweets').find('._row1').append(text);
 				}
 			}
 			$('#_tweets').find('._row2').html('');
-			for (var i in myTweets) {
-				if (i >= 3) {
-					var tweet = myTweets[i];
-					$('#_tweets').find('._row2').append('<div class="col-md-4">' +
-							'<p>' + tweet.full_name + ' @' + tweet.screen_name + '</p>' +
-							'<p>' + tweet.text + '</p>' +
-							'<p>' + tweet.url + '</p>' +
-							'<p>' + tweet.created_at + '</p>' +
-						'</div>');
+			for (i in myTweets) {
+				if (i < 3) {
+					tweet = myTweets[i];
+					text = '<div class="col-md-4">' +
+						'<p>$full_name @$screen_name</p>' +
+						'<p>$text</p>' +
+						'<p>$url</p>' +
+						'<p>$created_at</p>' +
+						'</div>';
+					for (k in tweet) {
+						text = text.replace("$"+k, tweet[k]);
+					}
+					$('#_tweets').find('._row2').append(text);
 				}
 			}
 		});
 	});
 });
+
+
+
+
