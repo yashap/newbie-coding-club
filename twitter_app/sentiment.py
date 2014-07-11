bbe435b484dcef6397f1a7c7e9a06e07eaad1e6b
yashap <ypodeswa@gmail.com>
2014-07-10 18:07:34 -0700

    Got basic sentiment analysis working with fake tweet

--- twitter_app/sentiment.py
+++ twitter_app/sentiment.py
@@ -7,3 +7,11 @@ def load():
 		for row in tsvin:
 			out[row[0]] = int(row[1])
 	return out
+
+test = load()
+
+tweet = "Greeting world my name is crap"
+tweet_list = tweet.split()
+
+for word in tweet_list:
+	print("%s %s" % (word, test.get(word)))
